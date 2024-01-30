27635c5b878c8985314d10d662df084337bc7ed6
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2024-01-30 10:31:16 +0100

    clean train.py

--- src/train.py
+++ src/train.py
@@ -2,22 +2,7 @@ from stable_baselines3 import PPO
 from custom_env import loadCustomEnv
 import glob
 import os
-"""def train(policy, env_path, env_hyperparameters,max_episode_length,seed_env,learning_rate,clip_range,ent_coef,n_steps,batch_size,
-          n_epochs, gamma,custom_reward_logging, tensorboard_log,device,verbose,tb_log_name,timesteps,seed_ppo,save_path):
-    # load the env
-    env = loadCustomEnv(env_path=env_path, hyperparameters =env_hyperparameters,max_episode_length = max_episode_length,seed=seed_env)
-    model = PPO(policy=policy, env=env, verbose=verbose,n_steps=n_steps, batch_size=batch_size,n_epochs = n_epochs,gamma = gamma,
-            learning_rate=learning_rate,clip_range = clip_range,ent_coef = ent_coef,tensorboard_log=tensorboard_log, device=device, seed= seed_ppo)
-    TIMESTEPS = timesteps
-    #model.learn(total_timesteps=TIMESTEPS,callback=CustomRewardLoggingCallback(),tb_log_name=tb_log_name)
-    #for i in range(totalTimesteps):
-    #    model.learn(total_timesteps=TIMESTEPS,reset_num_timesteps=False, callback=CustomRewardLoggingCallback(),tb_log_name=tb_log_name)
-    model.learn(total_timesteps=TIMESTEPS, callback=custom_reward_logging(),tb_log_name=tb_log_name)
-    # Store the model
-    model.save(save_path)
 
-    # close the env
-    env.close()"""
 
 def train(policy, env_path, env_hyperparameters, max_episode_length, seed_env, learning_rate, clip_range, ent_coef, n_steps, batch_size,
           n_epochs,target_kl,max_grad_norm, callback, tensorboard_log, device, verbose, tb_log_name, timesteps, seed_ppo, save_path):
@@ -25,7 +10,7 @@ def train(policy, env_path, env_hyperparameters, max_episode_length, seed_env, l
     env = loadCustomEnv(env_path=env_path, hyperparameters=env_hyperparameters, max_episode_length=max_episode_length, seed=seed_env)
     if target_kl == "None":
         target_kl = None
-    # Find the latest saved model
+    # Find the latest saved model, to continue training (in HPC for example after time limit) 
     list_of_files = glob.glob(os.path.join(save_path, '*.zip')) # Assumes the saved models are in .zip format
     if list_of_files:
         latest_file = max(list_of_files, key=os.path.getctime)
